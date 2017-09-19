7a8d33030d820dcba4e8379fec263649f7571041
Samuel Gomez <gressboldude@gmail.com>
2017-09-19 16:01:59 +0200

    added form input

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var nodeMailer = require("nodemailer");
 
 // allows the scripts from the public folder to be run
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 	res.render("index.ejs");
 });
+
+// mailing
+
 app.get("*", function(req, res){
 	res.redirect("/");
 });
-
-app.listen(process.env.PORT, process.env.IP);
+app.listen(3000);
+//app.listen(process.env.PORT, process.env.IP);
