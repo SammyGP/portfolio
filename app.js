239d51fe6720dde471ba8ecbdec4e3a955342ced
Samuel <gressboldude@gmail.com>
2017-09-20 19:33:45 +0200

    port issue

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.get("*", function(req, res){
 	res.redirect("/");
 });
 
-//app.listen(3000);
-app.listen(process.env.PORT, process.env.IP);
+app.listen(3000);
+//app.listen(process.env.PORT, process.env.IP);
